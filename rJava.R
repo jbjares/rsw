69b28f15eedc0e683b710f0ffb30328efadc8f02
jbjares <joao.jares@insight-centre.org>
2015-05-17 01:00:53 +0100

    - Dynamic JAVA_HOME definition - first version.

--- rJava.R
+++ rJava.R
@@ -1,8 +1,19 @@
-#TODO insert an inputdata to end-user set the JAVA_HOME path
-Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk1.7.0_71/jre")
 
-if(!("rJava" %in% rownames(installed.packages()))){
-        install.packages("rJava")  
+javaHomeVar <- readline("If you know where's located your JAVA_HOME, please set. Else just input 'N'? \n (e.g C:/Program Files/Java/jdk1.7.0_71/jre)") 
+if(javaHomeVar=="N"){
+  javaHomeVar <- readline(
+    "Can I set to the following location C:/Program Files/Java/jdk1.7.0_71/jre ? (Y/N)") 
+}
+if(javaHomeVar=="Y"){
+  Sys.setenv(JAVA_HOME=paste0(gsub("\\\\", "/", javaHomeVar)))
+}
+if(javaHomeVar==""){
+  print("Quitting app...")
+  q()
 }
 
-library(rJava)
+print("Thank you!")
+
+if(!("rJava" %in% rownames(installed.packages()))){
+  install.packages("rJava")  
+}
